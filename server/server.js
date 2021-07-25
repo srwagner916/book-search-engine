d5dda39c02a1a9856363fc37fe575a540e0dd5b2
Shay Wagner <srwagner916@gmail.com>
2021-07-25 16:33:42 -0500

    apollo server is implemented into express

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+// uncomment this out
+const { typeDefs, resolvers } = require('./schemas');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
@@ -6,16 +9,31 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+// create apollo server
+async function startApolloServer() {
+  const server= new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+  await server.start();
+  //integrate apollo server with express
+  server.applyMiddleware({ app });
+  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+      console.log(`Use GraphQl at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+startApolloServer();
